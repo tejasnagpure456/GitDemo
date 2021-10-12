023e2d9bf6051dddfa9d69019376ee2e0377fb64
tejas <tejasnagpure456@gmail.com>
2021-10-12 21:44:33 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	@Test
+	public void iosApps()
+	{
+		System.out.println("iosApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+		@Test
+		public void browserAutomation()
+		{
+			System.out.println("BrowserAutomation");
+		}
+		@Test
+		public void elementsUi()
+		{
+			System.out.println("elementsUi");
+		}
+}
